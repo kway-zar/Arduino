5bfb16c328b46ec1ab63d60614d6ffd6251768ba
kway-zar <137285693+kway-zar@users.noreply.github.com>
2023-09-27 20:06:41 +0800

    Beta

---
+++ Main/main.ino
@@ -0,0 +1,26 @@
+
+int day;
+
+int sec,mins,hrs;
+char timeAlarm_range1[]; //this are also strings
+char timeAlarm_range2[];
+
+bool deviceIsActive;
+bool dayCompleted;
+
+bool doMechanism;
+
+void setTime(int hours,int minutes, int seconds) {
+
+
+};
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
