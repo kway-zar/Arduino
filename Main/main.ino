e14349e13508130d3dc90441f680308f56366041
kway-zar <137285693+kway-zar@users.noreply.github.com>
2023-09-29 16:57:47 +0800

    Master

--- Main/main.ino
+++ Main/main.ino
@@ -1,42 +1,48 @@
 
 
 int day;
-
 int sec,mins,hrs;
+int LDRSensor;
+
 String timeAlarm_range1; //this are also strings
 String timeAlarm_range2;
+String timeAlarm_range3;
 
 bool deviceIsActive;
 bool dayCompleted;
+bool condition1;
+bool condition2;
 
-int LDRSensor;
-int LightRange1;
-int LightRange2;
-
-bool doMechanism;
+bool mechanismCompleted;
 
 
+#define LDRpin A0
 
 
 
 
 void setup() {
 
-    setTime(1,2,0);
-    timeAlarm_range1 = cvtTime_to_str(1,2,3);
-    timeAlarm_range2 = cvtTime_to_str(1,2,5);
+  pinMode(2,OUTPUT);
+
 
 }
 
 void loop() {
 
     Serial.begin(9600);
-    
     deviceIsActive = true;
 
+    LDRSensor = analogRead(LDRpin);
+ 
+    setTime(23,59,50);
+
     while(deviceIsActive !=false) {
+
         
 
+        timeAlarm_range1 = cvtTime_to_str(0,0,2);
+        timeAlarm_range2 = cvtTime_to_str(1,2,10);
 
         sec++;
         delay(1000);
@@ -49,6 +55,7 @@ void loop() {
         Serial.print(mins);
         Serial.print(":");
         Serial.print(sec);
+
 
 
         if(sec == 60) {
@@ -64,27 +71,38 @@ void loop() {
                   day++;
                   hrs = 0;
                 }
+                  
                   if(time == timeAlarm_range1 || time == timeAlarm_range2) {
-
-                      bool condition1 = true;
-
                       
 
+                      condition1 = true;
+                      
+                      Serial.println(" condition completed");
                       
 
-                      // if(LDRSensor <= LightRange1 && LDRSensor >= LightRange2) {
+                       if(LDRSensor > 700 || LDRSensor <= 800) {
+
+                          condition2 = true;
+                          Serial.println("condtion2 completed");
+
+                          doMech(condition1,condition2);
 
 
-                      // }
+
+                       }
                       
                   }
                   if(day == 1) {
 
-                    dayCompleted = true;
                     day = 0;
-                    } if (sec == 2) {
+                    
+                    } if (hrs == 0 && mins ==0 && sec == 0) {
 
                       dayCompleted = false;
+
+                      mechanismCompleted = false;
+
+                      Serial.println("day reset");
                     }
 
 
@@ -110,11 +128,22 @@ String cvtTime_to_str(int hrs, int mins, int sec) {
 
     cnvertdSTR = hours + ":" + minutes + ":" + sec;
 
+    return cnvertdSTR;
+
 };
 
 void doMech(bool condition1, bool condition2) {
     if(condition1 == true && condition2 == true) {
 
+        digitalWrite(2,HIGH);
+
+        dayCompleted = true;
+        mechanismCompleted = true;
+        
+
+        delay(3000);
+        sec +=3; // it equals to lost seconds
+        digitalWrite(2, LOW);
 
     }
 
